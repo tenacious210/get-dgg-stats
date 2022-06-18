833bdc92861c3119afbb46d54d3bf38b8885345a
tenacious210 <tenacious210@gmail.com>
2022-06-18 19:52:13 -0400

    Preserves date in database

--- emote_grabber.py
+++ emote_grabber.py
@@ -11,46 +11,58 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 
-def update_emote_stats():
+def update_emote_stats(
+    start_date: datetime = datetime.today(), end_date: datetime = None
+):
+    if not end_date:
+        end_date = start_date
     emote_json = requests.get("https://cdn.destiny.gg/emotes/emotes.json").json()
     emotes = [e["prefix"] for e in emote_json]
-    user_emotes = {}
 
-    today = date.today()
-    tomorrow = today + timedelta(days=1)
-    thirty_days_ago = today - timedelta(days=30)
-    for days in daterange(thirty_days_ago, tomorrow):
-        year, month_num, month_name, day = days.strftime("%Y %m %B %d").split()
-        rustle_url = f"https://dgg.overrustlelogs.net/Destinygg%20chatlog/{month_name}%20{year}/{year}-{month_num}-{day}.txt"
+    con = sqlite3.connect("user_emotes.db")
+    # Will create the .db file if it doesn't exist
+    cur = con.cursor()
+    cmd = (
+        "CREATE TABLE IF NOT EXISTS EmoteStats ("
+        "UserName STRING NOT NULL, "
+        "Date DATE NOT NULL)"
+    )
+    cur.execute(cmd)
+
+    columns = [i[1] for i in cur.execute("PRAGMA table_info(EmoteStats)")]
+    for emote in emotes:
+        if emote not in columns:
+            cur.execute(f"ALTER TABLE EmoteStats ADD {emote} INT")
+
+    next_day = end_date + timedelta(days=1)
+    for day in daterange(start_date, next_day):
+        user_emotes = {}
+        year_num, month_num, month_name, day_num = day.strftime("%Y %m %B %d").split()
+        rustle_url = (
+            "https://dgg.overrustlelogs.net/Destinygg%20chatlog/"
+            f"{month_name}%20{year_num}/{year_num}-{month_num}-{day_num}.txt"
+        )
         print(f"Connecting to {rustle_url}")
         logs = requests.get(rustle_url).text.split("\n")
         for log in logs:
-            if user_search := re.search(r"\[.*\] (\w*): ", log):
-                user = user_search.group(1)
+            if len(log) > 26:
+                user = log[26 : log.find(":", 26)]
                 if user not in user_emotes.keys():
                     user_emotes[user] = {emote: 0 for emote in emotes}
                 for emote in emotes:
                     user_emotes[user][emote] += len(re.findall(rf"\b{emote}\b", log))
-
-    emote_db_con = sqlite3.connect("user_emotes.db")
-    # Will create the .db file if it doesn't exist
-    emote_db_cur = emote_db_con.cursor()
-    emote_db_cur.execute(
-        "CREATE TABLE IF NOT EXISTS LastMonth (UserName STRING PRIMARY KEY NOT NULL UNIQUE)"
-    )
-    emote_db_cur.execute("DELETE FROM LastMonth")
-    columns = [i[1] for i in emote_db_cur.execute("PRAGMA table_info(LastMonth)")]
-    for emote in emotes:
-        if emote not in columns:
-            emote_db_cur.execute(f"ALTER TABLE LastMonth ADD {emote} INT")
-    db_keys = f"`UserName`{''.join([f',`{emote_name}`' for emote_name in emotes])}"
-    for username, emote_dict in user_emotes.items():
-        db_values = f"'{username}'" + "".join(
-            [f",'{emote_count}'" for emote_count in emote_dict.values()]
+        db_date = f"{year_num}-{month_num}-{day_num}"
+        db_keys = (
+            f"`UserName`,`Date`{''.join([f',`{emote_name}`' for emote_name in emotes])}"
         )
-        emote_db_cur.execute(f"INSERT INTO LastMonth ({db_keys}) VALUES ({db_values})")
-    emote_db_con.commit()
-    emote_db_con.close()
+        for username, emote_dict in user_emotes.items():
+            db_values = f"'{username}','{db_date}'" + "".join(
+                [f",'{emote_count}'" for emote_count in emote_dict.values()]
+            )
+            cur.execute(f"INSERT INTO EmoteStats ({db_keys}) VALUES ({db_values})")
+        con.commit()
+
+    con.close()
 
     storage_client = storage.Client()
     bucket = storage_client.bucket("tenadev")
@@ -61,4 +73,6 @@ def update_emote_stats():
 
 
 if __name__ == "__main__":
-    update_emote_stats()
+    update_emote_stats(
+        start_date=datetime.today() - timedelta(days=1), end_date=datetime.today()
+    )
