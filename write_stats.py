8fd9cddeb1be6a81ddde8f3a71b6f3a068000e6e
tenacious210 <tenacious210@gmail.com>
2022-09-12 10:08:10 -0400

    Now uses only one sql cursor

--- write_stats.py
+++ write_stats.py
@@ -5,8 +5,8 @@ import json
 
 
 def define_tables(return_users=False):
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
+    dt_con = sqlite3.connect("dgg_stats.db", timeout=60.0)
+    dt_cur = dt_con.cursor()
     commands = (
         "CREATE TABLE IF NOT EXISTS Lines ("
         "UserName STRING NOT NULL UNIQUE, "
@@ -29,33 +29,27 @@ def define_tables(return_users=False):
         "Bans STRING NOT NULL)",
     )
     for command in commands:
-        cur.execute(command)
+        dt_cur.execute(command)
     user_index = None
     if return_users:
         cmd = "SELECT UserName FROM Lines ORDER BY Amount DESC LIMIT 5000"
-        user_index = [str(i[0]) for i in cur.execute(cmd).fetchall()]
-    con.commit()
-    con.close()
+        user_index = [str(i[0]) for i in dt_cur.execute(cmd).fetchall()]
+    dt_con.commit()
+    dt_con.close()
     return user_index
 
 
-def check_latest_emotes():
+def check_latest_emotes(cur: sqlite3.Cursor):
     emote_json = requests.get("https://cdn.destiny.gg/emotes/emotes.json").json()
     emotes = [e["prefix"] for e in emote_json]
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
     columns = [i[1] for i in cur.execute("PRAGMA table_info(EmoteStats)")]
     for emote in emotes:
         if emote not in columns:
             cur.execute(f"ALTER TABLE EmoteStats ADD {emote} INT")
-    con.commit()
-    con.close()
     return emotes
 
 
-def add_lines(username, amount):
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
+def add_lines(username, amount, cur: sqlite3.Cursor):
     params = {"username": username, "amount": amount}
     commands = (
         "INSERT OR IGNORE INTO Lines (UserName, Amount) VALUES (:username, 0)",
@@ -63,13 +57,9 @@ def add_lines(username, amount):
     )
     for cmd in commands:
         cur.execute(cmd, params)
-    con.commit()
-    con.close()
 
 
-def update_mentions(username, mentions: dict):
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
+def update_mentions(username, mentions: dict, cur: sqlite3.Cursor):
     params = {"username": username}
     cmd = "INSERT OR IGNORE INTO UserMentions (UserName, Mentions) VALUES (:username, '{}')"
     cur.execute(cmd, params)
@@ -82,13 +72,9 @@ def update_mentions(username, mentions: dict):
     params["mentions"] = json.dumps(db_mentions)
     cmd = "UPDATE UserMentions SET Mentions = :mentions WHERE UserName = :username"
     cur.execute(cmd, params)
-    con.commit()
-    con.close()
 
 
-def update_emotes(username, date: datetime, emote_dict: dict):
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
+def update_emotes(username, date: datetime, emote_dict: dict, cur: sqlite3.Cursor):
     params = {"username": username}
     cmd = (
         f"INSERT OR IGNORE INTO EmoteStats "
@@ -97,15 +83,33 @@ def update_emotes(username, date: datetime, emote_dict: dict):
         f"{', '.join([str(v) for v in emote_dict.values()])})"
     )
     cur.execute(cmd, params)
-    con.commit()
-    con.close()
 
 
-def update_top_posters():
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
+def update_tng_score(username, change, cur: sqlite3.Cursor):
+    mode = "+" if change > 0 else "-"
+    params = {"username": username, "change": abs(change), "mode": mode}
+    commands = (
+        "INSERT OR IGNORE INTO TngScore (UserName, Score) VALUES (:username, 0)",
+        f"UPDATE TngScore SET Score = Score {mode} :change WHERE UserName = :username",
+    )
+    for cmd in commands:
+        cur.execute(cmd, params)
+
+
+def update_bans(username, new_banlist: list, cur: sqlite3.Cursor):
+    params = {"username": username}
+    cmd = "INSERT OR IGNORE INTO UserBans (UserName, Bans) VALUES (:username, '[]')"
+    cur.execute(cmd, params)
+    cmd = "SELECT Bans FROM UserBans WHERE UserName = :username"
+    old_banlist = json.loads(cur.execute(cmd, params).fetchall()[0][0])
+    params["banlist"] = json.dumps(old_banlist + new_banlist)
+    cmd = f"UPDATE UserBans SET Bans = :banlist WHERE UserName = :username"
+    cur.execute(cmd, params)
+
+
+def update_top_posters(cur: sqlite3.Cursor):
     cur.execute("DELETE FROM TopPosters")
-    emotes = check_latest_emotes()
+    emotes = check_latest_emotes(cur)
     target_day = datetime.today() - timedelta(days=30)
     sql_date = target_day.strftime("%Y-%m-%d")
     for emote in emotes:
@@ -120,56 +124,24 @@ def update_top_posters():
         params = {"emote": emote, "posters": top_posters}
         cmd = "INSERT INTO TopPosters VALUES (:emote, :posters)"
         cur.execute(cmd, params)
-    con.commit()
-    con.close()
-
-
-def update_tng_score(username, change):
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
-    mode = "+" if change > 0 else "-"
-    params = {"username": username, "change": abs(change), "mode": mode}
-    commands = (
-        "INSERT OR IGNORE INTO TngScore (UserName, Score) VALUES (:username, 0)",
-        f"UPDATE TngScore SET Score = Score {mode} :change WHERE UserName = :username",
-    )
-    for cmd in commands:
-        cur.execute(cmd, params)
-    con.commit()
-    con.close()
-
-
-def update_bans(username, new_banlist: list):
-    con = sqlite3.connect("dgg_stats.db")
-    cur = con.cursor()
-    params = {"username": username}
-    cmd = "INSERT OR IGNORE INTO UserBans (UserName, Bans) VALUES (:username, '[]')"
-    cur.execute(cmd, params)
-    cmd = "SELECT Bans FROM UserBans WHERE UserName = :username"
-    old_banlist = json.loads(cur.execute(cmd, params).fetchall()[0][0])
-    params["banlist"] = json.dumps(old_banlist + new_banlist)
-    cmd = f"UPDATE UserBans SET Bans = :banlist WHERE UserName = :username"
-    cur.execute(cmd, params)
-    con.commit()
-    con.close()
 
 
 def write_dgg_stats(stats, date: datetime):
-    print("Writing lines...")
+    con = sqlite3.connect("dgg_stats.db", timeout=60.0)
+    cur = con.cursor()
+    print("Writing stats...")
     for username, amount in stats["lines"].items():
-        add_lines(username, amount)
-    print("Writing emotes...")
-    print("Writing mentions...")
+        add_lines(username, amount, cur)
     for username, mentions in stats["mentions"].items():
-        update_mentions(username, mentions)
-    print("Writing bans...")
+        update_mentions(username, mentions, cur)
     for username, banlist in stats["bans"].items():
-        update_bans(username, banlist)
-    print("Writing tng scores...")
+        update_bans(username, banlist, cur)
     for username, change in stats["tng_score"].items():
-        update_tng_score(username, change)
-    check_latest_emotes()
+        update_tng_score(username, change, cur)
+    check_latest_emotes(cur)
     for username, emote_stats in stats["emotes"].items():
-        update_emotes(username, date, emote_stats)
+        update_emotes(username, date, emote_stats, cur)
     print("Writing top posters...")
-    update_top_posters()
+    update_top_posters(cur)
+    con.commit()
+    con.close()
